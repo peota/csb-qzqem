a2d5e93c7d005d74154b4f85f8917859af00a380
Code Sandbox <hello@codesandbox.io>
2021-05-27 14:15:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4e60f95118a004dbc38e6a598b09439f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "391af091d5c584c384f9",
+    "url": "https://.github.io/csb-qzqem/static/css/main.ab117a31.chunk.css"
+  },
+  {
+    "revision": "391af091d5c584c384f9",
+    "url": "https://.github.io/csb-qzqem/static/js/main.391af091.chunk.js"
+  },
+  {
+    "revision": "dac3eb639cd222ff8861",
+    "url": "https://.github.io/csb-qzqem/static/js/1.dac3eb63.chunk.js"
+  },
+  {
+    "revision": "8e603575dde84b2e78a7",
+    "url": "https://.github.io/csb-qzqem/static/js/runtime~main.8e603575.js"
+  },
+  {
+    "revision": "de3cbad845a6d74f1c993576422e3a4d",
+    "url": "https://.github.io/csb-qzqem/index.html"
+  }
+];
